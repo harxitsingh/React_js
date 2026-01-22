4f4b6652eac85eae286c07f2d69807e30f89b230
Harshit Singh <harxitsingh@gmail.com>
2026-01-22 21:44:25 +0530

    Uncomment App component and enhance food list

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -1,169 +1,171 @@
-// import React from "react" ;
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
+import React from "react" ;
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-// function App (){
+function App (){
 
-//   return (
-//    <React.Fragment>
-//                  {/* or this shortcut for  react Fragment  <>    </> */}
-//     <h1> Healthy Food </h1>
-//     <ul className = "list-group">
-//       <li className = "ListGroupitem"> dal</li>
-//       <li className = "ListGroupitem"> green veg</li>
-//       <li className = "ListGroupitem"> fruits</li>
-//       <li className = "ListGroupitem">soup </li>
-//     </ul>
-//   </React.Fragment>
-//   );
-// }
-// export default App;
-
-
-// --------------map===================
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import './App.css'
-
-// function App() {
-//   let Fooditems = ['dal','sabji' ,'roti','soup'];
-
-//   return (
-//     <>
-//       <h1>Healthy Food</h1>
-
-//       <ul className="list-group">
-//         {Fooditems.map((item) => {
-//           return (
-//             <li className="list-group-item" key={item}>
-//               {item}
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default App;
+  return (
+   <React.Fragment>
+                 {/* or this shortcut for  react Fragment  <>    </> */}
+    <h1> Healthy Food </h1>
+    <ul className = "list-group">
+      <li className = "ListGroupitem"> dal</li>
+      <li className = "ListGroupitem"> green veg</li>
+      <li className = "ListGroupitem"> fruits</li>
+      <li className = "ListGroupitem">soup </li>
+      <li className = "ListGroupitem">Milk </li>
+            
+    </ul>
+  </React.Fragment>
+  );
+}
+export default App;
 
 
-//----------conditional Rendering ----------
-// -- Three ways - 1. if else    2. ternery     t 3.logical operators
+--------------map===================
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+  let Fooditems = ['dal','sabji' ,'roti','soup'];
+
+  return (
+    <>
+      <h1>Healthy Food</h1>
+
+      <ul className="list-group">
+        {Fooditems.map((item) => {
+          return (
+            <li className="list-group-item" key={item}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
 
 
-//1. if else  only i am still hungry likhega
-// import "bootstrap/dist/css/bootstrap.min.css"; // for better list ui - bootstrao 
-// function App() {
-// let Fooditems = ['dal','sabji' ,'roti','soup'];
+----------conditional Rendering ----------
+-- Three ways - 1. if else    2. ternery     t 3.logical operators
+
+
+1. if else  only i am still hungry likhega
+import "bootstrap/dist/css/bootstrap.min.css"; // for better list ui - bootstrao 
+function App() {
+let Fooditems = ['dal','sabji' ,'roti','soup'];
 
 
 
-// if ( Fooditems.length === 0){
-//   return <h3>I am still hungry</h3>
+if ( Fooditems.length === 0){
+  return <h3>I am still hungry</h3>
 
-// }
+}
 
-//   return (
-//     <>
-//       <h1>Healthy Food</h1>
-//       {}
-//       <ul className="list-group">
-//         {Fooditems.map((item) => {
-//           return (
-//             <li className="list-group-item" key={item}>
-//               {item}
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      {}
+      <ul className="list-group">
+        {Fooditems.map((item) => {
+          return (
+            <li className="list-group-item" key={item}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
 
-// export default App;
+export default App;
 
-// // 2. ternery  // heading ke andar i am still hungry likhega 
+// 2. ternery  // heading ke andar i am still hungry likhega 
 
-// function App() {
+function App() {
 
-//   let Fooditems = ['dal','sabji','roti','soup'];
+  let Fooditems = ['dal','sabji','roti','soup'];
 
-//     //  or let emptyMessage =
-//   //    Fooditems.length === 0 ? <h3> i am still hungry</h3> : null;
+    //  or let emptyMessage =
+  //    Fooditems.length === 0 ? <h3> i am still hungry</h3> : null;
 
-//   return (
-//     <>
-//       <h1>Healthy Food</h1>
-//  {/* or  {emptyMessage}  */}
-//       {Fooditems.length ===0 ? <h3> i am still hungry .</h3> : null}
+  return (
+    <>
+      <h1>Healthy Food</h1>
+ {/* or  {emptyMessage}  */}
+      {Fooditems.length ===0 ? <h3> i am still hungry .</h3> : null}
         
 
-//       <ul className="list-group">
-//         {Fooditems.map((item) => {
-//           return (
-//             <li className="list-group-item" key={item}>
-//               {item}
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </>
-//   );
-// }
+      <ul className="list-group">
+        {Fooditems.map((item) => {
+          return (
+            <li className="list-group-item" key={item}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
 
-// export default App;
+export default App;
 
-// 3. logical operators 
+3. logical operators 
 
-// function App() {
+function App() {
 
-//   let Fooditems = ['dal','sabji','roti','soup'];
+  let Fooditems = ['dal','sabji','roti','soup'];
 
-//   return (
-//     <>
-//       <h1>Healthy Food</h1>
+  return (
+    <>
+      <h1>Healthy Food</h1>
 
-//       {Fooditems.length ===0 && <h3> i am still hungry .</h3>}
+      {Fooditems.length ===0 && <h3> i am still hungry .</h3>}
 
-//       <ul className="list-group">
-//         {Fooditems.map((item) => {
-//           return (
-//             <li className="list-group-item" key={item}>
-//               {item}
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </>
-//   );
-// }
-// export default App;
+      <ul className="list-group">
+        {Fooditems.map((item) => {
+          return (
+            <li className="list-group-item" key={item}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+export default App;
 
-// // -------props ------------
-// import FoodItems from "./Components/FoodItems";
-// import ErrorMessage from "./Components/ErrorMessage";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css"; 
-// import Container from "./Components/Container";
+// -------props ------------
+import FoodItems from "./Components/FoodItems";
+import ErrorMessage from "./Components/ErrorMessage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"; 
+import Container from "./Components/Container";
 
-// function App() {
-//   let FoodItemsData = ['sabji', 'green veg', 'roti', 'salad'];
+function App() {
+  let FoodItemsData = ['sabji', 'green veg', 'roti', 'salad'];
 
-//   return (
-// <Container>
-//   <h1 className="food-heading">Healthy Food</h1>
+  return (
+<Container>
+  <h1 className="food-heading">Healthy Food</h1>
 
-//   <ErrorMessage items={FoodItemsData} />
+  <ErrorMessage items={FoodItemsData} />
   
-//   <FoodItems items={FoodItemsData} />
+  <FoodItems items={FoodItemsData} />
   
 
-//   <p>Above is the list of healthy foods...</p>
-// </Container>
+  <p>Above is the list of healthy foods...</p>
+</Container>
 
       
-//   );
-// }
+  );
+}
 
-// export default App;
+export default App;
